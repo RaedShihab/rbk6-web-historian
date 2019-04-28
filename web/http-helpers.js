3825ef85d78fc3a2be51f7480aec00ca92dd2c1b
MohammadElokour <mfelokour@gmail.com>
2019-04-28 20:15:45 +0300

    correction

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -4,7 +4,7 @@ var archive = require('../helpers/archive-helpers');
 
 exports.headers = {
   'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, UPOST, PT, DELETE, OPTIONS',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10, // Seconds.
   'Content-Type': 'text/html'
